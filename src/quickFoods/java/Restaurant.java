99a43eadf756bc1cc3d524e35f5f726b5ee71a37
Casyoo <casper.struwig@gmail.com>
2023-07-04 15:15:45 +0200

    Added more comments to addNewRestaurant function

--- src/quickFoods/java/Restaurant.java
+++ src/quickFoods/java/Restaurant.java
@@ -13,10 +13,13 @@ public class Restaurant extends GeneralInformation {
 	static Restaurant addNewRestaurant(){
 		//Scanner to obtain customer information required for constructor 
 		Scanner userInput = new Scanner(System.in);
+		//get user input for restaurant name
 		System.out.println("Please enter restaurant name.");
 		String name = userInput.nextLine();
+		//get user input for restaurant contact number
 		System.out.println("Please enter restaurant contact number.");
 		String contactNumber = userInput.nextLine();
+		//get user input for restaurant location
 		System.out.println("Please enter the location of the restaurant?(Capitalized first letter please)");
 		String location = userInput.nextLine();
 		//Constructor to create new restaurant object
